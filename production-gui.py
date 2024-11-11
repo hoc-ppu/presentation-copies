baff5874f8d0b881e490037bf9d5d2f013f4770b
adelei-hansard <simmondsn@parliament.uk>
2024-11-11 12:16:06 +0000

    added checks, error and warning labels

--- production-gui.py
+++ production-gui.py
@@ -1,15 +1,6 @@
-import os
 import tkinter as tk
-from tkinter import *
-from tkinter import filedialog, ttk
-
+from tkinter import filedialog, ttk, messagebox
 import speech_functions
-
-## some tests to do after demo ##
-
-# # try removing the tk ttk - are they duplications?
-# # the file dialog seemed to only work with tk?
-
 
 
 # Function for opening folder directory
@@ -18,49 +9,57 @@ def browse_folder():
     if folder_path:
         folder_text.set(folder_path)
 
+
 # cannot call function which has parameters, so create a function with none
 # then can call on function with parameters
 # calls the speech script so that we can add the URL and file path
 def save():
-    speech_functions.get_speech(url.get(), folder_text.get())
-    tk.messagebox.showinfo(title="All Done", message="All Done")
+    try:
+        warn_tuple = speech_functions.get_speech(url.get(), folder_text.get())
+        if any(warn_tuple):
+            messagebox.showwarning(title="Warning", message=", ".join(warn_tuple))
+    except Exception as e:
+        messagebox.showerror(title="Error", message=str(e))
+    else:
+        messagebox.showinfo(title="All Done", message="All Done")
+
 
 # creating the GUI root
-root = Tk()
+root = tk.Tk()
 root.title("Presentation Copies | Download and Save Content")
 
 # creating the window
 mainframe = ttk.Frame(root, padding="3 3 12 12")
-mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+mainframe.grid(column=0, row=0, sticky=(tk.N, tk.W, tk.E, tk.S))
 root.columnconfigure(0, weight=1)
 root.rowconfigure(0, weight=1)
 
-# input field - would text widget be better so can wrap depending on size of URL
-url = StringVar()
+# input field - would text widget be better so can wrap
+# depending on size of URL
+url = tk.StringVar()
 url_entry = ttk.Entry(mainframe, width=100, textvariable=url)
-url_entry.grid(column=2, row=1, sticky=(W, E))
-url.set("https://hansard.parliament.uk/Commons/2024-10-10/debates/4D26F1DB-5194-49CD-85CF-087377B7A3DB/SportTeamGBAndParalympicsgb#contribution-B7C30195-448D-4087-9106-EDDC8FC517B7")
+url_entry.grid(column=2, row=1, sticky=(tk.W, tk.E))
+url.set(
+    "https://hansard.parliament.uk/Commons/2024-10-10/debates/4D26F1DB-5194-49CD-85CF-087377B7A3DB/SportTeamGBAndParalympicsgb#contribution-B7C30195-448D-4087-9106-EDDC8FC517B7"
+)
 
 # label for input field
-ttk.Label(mainframe, text="Website Link (URL)").grid(column=1, row=1, sticky=W)
-
+ttk.Label(mainframe, text="Website Link (URL)").grid(column=1, row=1, sticky=tk.W)
 
 # Create a button to browse for a folder
-browse_button = ttk.Button(mainframe, text="Choose Folder", command=browse_folder).grid(column=3, row=2, sticky=W)
-
-# label for browse file manager field -
-#ttk.Label(mainframe, text="Select A Folder").grid(column=1, row=2, sticky=W)
+ttk.Button(mainframe, text="Choose Folder", command=browse_folder).grid(
+    column=3, row=2, sticky=tk.W
+)
 
 # entry / text field for the folder directory /file path
 folder_text = tk.StringVar()
 folder = tk.Entry(mainframe, textvariable=folder_text)
 new_text = "folder file path"
 folder_text.set(new_text)
-folder.grid(column=2, row=2, sticky=(W, E))
-
+folder.grid(column=2, row=2, sticky=(tk.W, tk.E))
 
 # execute button
-ttk.Button(mainframe, text="Finish", command=save).grid(column=2, row=3, sticky=W)
+ttk.Button(mainframe, text="Finish", command=save).grid(column=2, row=3, sticky=tk.W)
 
 # adding padding around widgets
 for child in mainframe.winfo_children():
--- speech_functions.py
+++ speech_functions.py
@@ -3,111 +3,173 @@ from pathlib import Path
 
 import requests
 from lxml import etree
+from lxml.etree import SubElement
+
+MISSING_TITLE = "INSERT TITLE"
 
 # this function is the same as the presentation copies script
 # turned into a function to use with tkinter GUI
 
-def get_speech(share_link: str, output_folder: str):
+# TODO: return line 46 and any others, create separate functions
+# TODO: add instruction to screenshot/report error to documentation
+# TODO: add raised exceptions as "e" for logger?
+
+
+def get_speech(share_link: str, output_folder: str) -> tuple[str, str, str]:
     # inserts link from production-gui
+    # TODO: has user entered expected format
 
-    # splitting the share link #contribution-
-    string_split = share_link.split("#contribution-")
+    # TODO: check if string_split is array with at least 2 elements
+    try:
+        # splitting the share link #contribution-
+        url_string_split = share_link.split("#contribution-")
+        # second value out of a list of two 0,1
+        content_item_external_id = url_string_split[1]
+    except Exception:
+        raise Exception("Website Link(URL)is invalid, check link and try again")
 
-    # second value out of a list of two 0,1
-    content_item_external_id = string_split[1]
-
-    # creating a variable which adds the string from above which is the external ID
-    find_debate_section_id_url = "https://hansard-api.parliament.uk/search/debatebyexternalid.json?contentItemExternalId={content_item_external_id}&house=Commons"
-
+    # creating a variable which adds the string from above which is the
+        # external ID
+    ext_id_url = (
+        "https://hansard-api.parliament.uk/search/"
+        "debatebyexternalid.json?contentItemExternal"
+        f"Id={content_item_external_id}&house=Commons"
+    )
     # formatting the variable with the externalid
-    ext_id_url = ((find_debate_section_id_url.format(content_item_external_id=content_item_external_id)))
+    try:
+        # requesting the new URL
+        response = requests.get(ext_id_url)
+        # creating a variable with the response from the above request
+        DebateSectionExtId_data = response.json()
+    except Exception:
+        raise Exception(f"Search hansard api URL failed{ext_id_url}. Check for typos in the pasted link and try again")
 
-    # requesting the new URL
-    response = requests.get(ext_id_url)
-
-    # creating a variable with the response from the above request
-    DebateSectionExtId_data = response.json()
+    DebateSectionExtId = ""
 
     # use the response to get the DebateSectionExtId value
     for item in DebateSectionExtId_data.get("Results", []):
-        # TODO: Handle cases where there are no results
         DebateSectionExtId = item.get("DebateSectionExtId")
+    # TODO: does this ever get raised? Does function above return NoneType?
+    if not DebateSectionExtId:
+        raise Exception(f"Unable to find DebateSectionExtId in search results {ext_id_url}. Check for typos pasted link and try again")
 
-    # create URL that fetches the maiden speech content and the title of the speech
+    # create URL that fetches the maiden speech content and the
+    # title of the speech
+    create_url_template = "https://hansard-api.parliament.uk/debates/debate/{}.json"
+    url = create_url_template.format(DebateSectionExtId)
 
-    create_url = "https://hansard-api.parliament.uk/debates/debate/{DebateSectionExtId}.json"
+    # fetching content from hansard API, program ends with error
+    try:
+        response_api = requests.get(url)
+        # creating a variable with the response from the above request
+        maiden_speech_data = response_api.json()
+    except Exception:
+        raise Exception(
+            f"Problem getting data from the API https://hansard-api.parliament.uk/debates/debate/"
+        )
 
-    url = ((create_url.format(DebateSectionExtId=DebateSectionExtId)))
+    overview = maiden_speech_data.get("Overview", {})
 
-    # requesting the new URL
-    response_api = requests.get(url)
+    debate_title = ""
 
-    # creating a variable with the response from the above request
-    maiden_speech_data = response_api.json()
+    # TODO: is a loop necessary? Can we get it directly?
+    debate_title = overview.get("Title", "INSERT TITLE")
+    if debate_title == "INSERT TITLE":
+        warn_title = "No title found in the json"
 
-    title = maiden_speech_data.get("Overview", [])
+    # TODO: look at using logger instead of below
+    warn_title = ""
+    warn_datetime = ""
+    warn_member_details = ""
 
-    #
+    # Set default values of below items to none and empty
+    # Allows program to run with empty returns
+
+    time_code = None
+    member_details = None
+    speech = ""
+    ext_id_found = False
+
     for item in maiden_speech_data.get("Items", []):
         if item.get("ExternalId") == content_item_external_id:
-            speech = item.get("Value")
-            debate_title = title.get("Title")
+            ext_id_found = True
+            # added boolean check for error if contentID is not found in
+            # results
+            speech = item.get("Value", "")
+            if speech == "":
+                raise Exception("Speech is missing from data")
             time_code = item.get("Timecode")
             member_details = item.get("AttributedTo")
+            break
+    # TODO: need to add multiple contributions and then
+    #       concatenate them together.
+
+    if ext_id_found is False:
+        raise Exception("Check for a blank space or typo at end of URL and try again. ContentExternalID not found")
 
     # adding extra info that can be automated later
     chamber = "House of Commons"
     speech_type = "The Maiden Speech"
 
-    # split atrributedto value into member name and details of member
-    member_name, cons, party = member_details.split(' (')
-    cons = cons.rstrip(')')  # Remove the trailing parenthesis
-    party = party.rstrip(')')  # Remove the trailing parenthesis
+    if member_details:
+        # split attributed to value into member name and details of member
+        member_name, cons, party = member_details.split(" (")
+        cons = cons.rstrip(")")  # Remove the trailing parenthesis
+        party = party.rstrip(")")  # Remove the trailing parenthesis
+    else:
+        member_name = "INSERT MEMBER NAME"
+        cons = "INSERT CONSTITUENCY"
+        party = "PARTY"
+        warn_member_details = "Can't find member details"
 
     # convert time_code into datetime
-    dt = datetime.fromisoformat(time_code)
-
-    # re-format the date and time
-    formatted_date = dt.strftime("%d %B %Y")
-
-    hour = dt.strftime("%I").lstrip("0")  # Get hour and remove leading zero
-    formatted_time = f"{hour}.{dt.strftime('%M').lstrip('0')}{dt.strftime('%p').lower()}"
+    if time_code:
+        dt = datetime.fromisoformat(time_code)
+        formatted_date = dt.strftime("%d %B %Y")
+        hour = dt.strftime("%I").lstrip("0")  # Get hour and remove leading zero
+        formatted_time = (
+            f"{hour}.{dt.strftime('%M').lstrip('0')}{dt.strftime('%p').lower()}"
+        )
+    else:
+        formatted_date = "INSERT DATE"
+        formatted_time = "INSERT TIME"
+        warn_datetime = "Can't find the date or time in the JSON"
 
     # creating a list which splits on line break and removes empty paragraphs
     para_list = [para for para in speech.split("\n") if para.strip()]
 
     # Creating XML output, start with root
-    output_element = etree.Element('root')
+    output_element = etree.Element("root")
 
-    speech_type_ele = etree.SubElement(output_element, 'speech_type')
+    speech_type_ele = SubElement(output_element, "speech_type")
     speech_type_ele.text = speech_type.strip()
 
-    member_element = etree.SubElement(output_element, 'Member')
+    member_element = SubElement(output_element, "Member")
     member_element.text = member_name.strip()
 
-    chamber_ele = etree.SubElement(output_element, 'chambers')
+    chamber_ele = SubElement(output_element, "chambers")
     chamber_ele.text = chamber.strip()
 
-    time_element = etree.SubElement(output_element, 'date')
+    time_element = SubElement(output_element, "date")
     time_element.text = formatted_date.strip()
 
     # adding title element
-    title_element = etree.SubElement(output_element, 'hs_2GenericHdg')
+    title_element = SubElement(output_element, "hs_2GenericHdg")
     title_element.text = debate_title.strip()
 
     # adding timecode element
-    time_element = etree.SubElement(output_element, 'hs_Timeline')
+    time_element = SubElement(output_element, "hs_Timeline")
     time_element.text = formatted_time.strip()
 
     # Create the member_details element
-    member_details = etree.SubElement(output_element, "member_details")
+    member_details = SubElement(output_element, "member_details")
 
     # Create and add the bold element for member name
-    bold_element = etree.SubElement(member_details, "bold")
+    bold_element = SubElement(member_details, "bold")
     bold_element.text = member_name
 
     # Create the details element for constituency and party
-    details_element = etree.SubElement(member_details, "details")
+    details_element = SubElement(member_details, "details")
     details_element.text = f" ({cons}) ({party})"
 
     # adding tag to each paragraph which is currently in a list
@@ -115,7 +177,7 @@ def get_speech(share_link: str, output_folder: str):
 
         tag_name = "hs_Para"
 
-        if paragraph.startswith('“'):
+        if paragraph.startswith("“"):
             tag_name = "hs_brev"
 
         speech_element = etree.fromstring(
@@ -132,11 +194,15 @@ def get_speech(share_link: str, output_folder: str):
 
     # gets member name for file name
     member_name_split = member_name.split()
-    # TODO: Handle cases where the member has multiple first names or last names
     firstname = member_name_split[0]
     lastname = member_name_split[1]
 
     output_file_path = Path(output_folder, f"{firstname}_{lastname}.xml")
 
     output_tree = etree.ElementTree(output_element)
-    output_tree.write(str(output_file_path), encoding="utf-8")
+    output_tree.write(
+        str(output_file_path), encoding="utf-8", xml_declaration=True
+    )
+
+    # TODO: is this the best way, maybe use logger?
+    return (warn_datetime, warn_title, warn_member_details)
