0f6625b24a9e9334ea2597e747b6410e8a248e09
adelei-hansard <simmondsn@parliament.uk>
2024-12-16 13:59:58 +0000

    Fix conflicts (#2)

--- production-gui.py
+++ production-gui.py
@@ -27,7 +27,8 @@ def save():
 
 # creating the GUI root
 root = tk.Tk()
-root.title("Presentation Copies | Download and Save Content")
+root.title("Presentation Copies | Download and Save Content [V1]")
+
 
 # creating the window
 mainframe = ttk.Frame(root, padding="3 3 12 12")
@@ -36,34 +37,37 @@ mainframe.grid(column=0, row=0, sticky="nsew")
 root.columnconfigure(0, weight=1)
 root.rowconfigure(0, weight=1)
 
+ttk.Label(mainframe, text="Insert the website URL from Click Up into the box below").grid(column=1, columnspan=2, row=1, sticky="nw")
+
 # input field - would text widget be better so can wrap
 # depending on size of URL
 url = tk.StringVar()
 url_entry = ttk.Entry(mainframe, width=100, textvariable=url)
 # url_entry.grid(column=2, row=1, sticky=(tk.W, tk.E))
-url_entry.grid(column=2, row=1, sticky="we")
-url.set(
-    "https://hansard.parliament.uk/Commons/2024-10-10/debates/4D26F1DB-5194-49CD-85CF-087377B7A3DB/SportTeamGBAndParalympicsgb#contribution-B7C30195-448D-4087-9106-EDDC8FC517B7"
-)
+
+url_entry.grid(column=2, row=2, sticky="we")
+url.set("")
 
 # label for input field
-ttk.Label(mainframe, text="Website Link (URL)").grid(column=1, row=1, sticky=tk.W)
+ttk.Label(mainframe, text="Website Link (URL)").grid(column=1, row=2, sticky=tk.W)
 
 # Create a button to browse for a folder
 ttk.Button(mainframe, text="Choose Folder", command=browse_folder).grid(
-    column=3, row=2, sticky=tk.W
+    column=3, row=3, sticky=tk.W
 )
 
 # entry / text field for the folder directory /file path
 folder_text = tk.StringVar()
 folder = tk.Entry(mainframe, textvariable=folder_text)
-new_text = "folder file path"
+new_text = ""
 folder_text.set(new_text)
 # folder.grid(column=2, row=2, sticky=(tk.W, tk.E))
-folder.grid(column=2, row=2, sticky="we")
+
+folder.grid(column=2, row=3, sticky="we")
 
 # execute button
-ttk.Button(mainframe, text="Finish", command=save).grid(column=2, row=3, sticky=tk.W)
+ttk.Button(mainframe, text="Finish", command=save).grid(column=2, row=4, sticky=tk.W)
+
 
 # adding padding around widgets
 for child in mainframe.winfo_children():
